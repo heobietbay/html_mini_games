08e7c28a0328df1148aff510e9c60fa66438cce6
Tran Dang Khoa <trandangkhoa@TMA.COM.VN>
2017-01-23 18:27:47 +0700

    Draw a T block

--- tetris/main.js
+++ tetris/main.js
@@ -1,20 +1,45 @@
+const SCALE = 20;
 const mainBoard = document.getElementById('board');
-const ctx = mainBoard.getContext('2d');;
+const ctx = mainBoard.getContext('2d');
+ctx.scale(SCALE,SCALE);
+
+var TShape = { pos: { x: 0, y : 0} , 
+               fillStyle: 'red',
+               matrix:[[0,0,0] ,
+					   [1,1,1] ,
+					   [0,1,0] ] }; 
+
 
 init();
+window.setInterval( () => window.requestAnimationFrame(update), 66);
 
-function draw()
+function update()
+{	
+    console.log("Calling draw TShape");
+    draw(TShape);    
+}
+
+function draw(shape)
 {
-	console.log("Calling draw...");
+	ctx.fillStyle = shape.fillStyle;	
+	var pos = shape.pos;
+	var matrix = shape.matrix;
+
+	matrix.forEach((row,y) => {
+		row.forEach((col,x) => {
+			if(col !== 0 ){	  
+               ctx.fillRect(x + pos.x, y + pos.y, 1, 1);
+			}
+		});
+	} );	
 }
 function initBoard()
 {
 	ctx.fillStyle = "rgba(0, 0, 200, 0.3)";
-    ctx.fillRect (0, 0, 480, 640);
+    ctx.fillRect (0, 0, 240, 320);
 }
 function init()
 {
-	initBoard();
-	draw();
-	requestAnimationFrame(draw);
+    initBoard();
+    update();
 }
